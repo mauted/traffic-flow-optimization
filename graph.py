931af6a46c6cd8aa958451b57eb93d9676a209a4
Luisa <103768169+luisa-li@users.noreply.github.com>
2024-11-08 15:26:23 -0500

    move and simulation

--- graph.py
+++ graph.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from collections import namedtuple
 import numpy as np
+from build_graph import make_spanning_tree, make_graph
+import random
 
 Edge = namedtuple('Edge', ['start', 'end'])
 
@@ -30,5 +32,33 @@ class Node:
     
 class Graph:
 
-    def __init__(self, num_nodes, num_edges):
-        pass
+    def __init__(self, num_nodes):
+        tree = make_spanning_tree(num_nodes)
+        self.nodes, self.edges = make_graph(tree)
+
+    def generate_random_path(self) -> list[Node]:
+        start, end = random.sample(self.nodes, 2)
+        return self.dfs(start, end)
+
+    def dfs(self, start: Node, end: Node) -> list[Node]:
+        """
+        Returns a path from start to end using depth-first search.
+        """
+        stack = [(start, [start])]
+        visited = set()
+
+        while stack:
+            node, path = stack.pop()
+            if node in visited:
+                continue
+            visited.add(node)
+
+            if node == end:
+                return path  # Return the path if end node is found
+
+            # Explore neighbors by extending the current path
+            for neighbor in node.outgoing:
+                if neighbor not in visited:
+                    stack.append((neighbor, path + [neighbor]))
+
+        return []  # Return an empty list if no path is found
